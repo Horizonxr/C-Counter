31b9dd8f21a4c4a1fba787664a6f93666df6b9ab
Horizonxr <1176542271@qq.com>
2021-09-16 14:04:14 +0800

    可以识别单层的switch结构，下一步打算通过堆栈来实现嵌套结构的识别

--- 源.cpp
+++ 源.cpp
@@ -5,26 +5,50 @@
 #include <vector>
 using namespace std;
 map<string, int> MapKeyGuan;
-
+vector<string> Zu;
+vector<int> case_counter;
+int ifelse_counter = 0;
+int if_elseif_else_counter = 0;
 int MyCut(char j) {
-	if (j == ' ' || j == '(' || j == ')' || j == '{' || j == '}' || j == ':' 
-		|| j == '\n' || j == ';' || j == '#' || j == '=' || j == '!' || j == '?' 
-		|| j == '-' || j == '<' || j == '>' || (j <=57 &&j>=48)) return 0;
+	if (j == ' ' || j == '(' || j == ')' || j == ':'
+		|| j == '\n' || j == ';' || j == '#' || j == '=' || j == '!' || j == '?'
+		|| j == '-' || j == '<' || j == '>' || (j <= 57 && j >= 48)) return 0;
+	else if (j == '{' || j == '}') return 2;
 	else return 1;
 }
 void InitMap() {
 	string KeyGuan[32] = { "auto","break","case","char","const","continue","default","do",
 		"double","else","enum","extern","float","for","goto","if","int","long","register","return","short","signed",
 		"sizeof","static","struct","switch","typedef","union","unsigned","void","volatile","while" };
-	for (int i = 0;i<32;i++)
+	for (int i = 0; i < 32; i++)
 		MapKeyGuan.insert(pair<string, int>(KeyGuan[i], 0));
+}
+void SwitchIfCounter() {
+	int flag = 0;
+	int caseCount = 0;
+	int ifFlag = 0;//Flag=0表示只有if，1表示if-else,2表示if-elseif-else
+	for (int i = 0; i < Zu.size(); i++) {
+		if (Zu[i] == "switch") {
+			for (int j = i; j <= Zu.size(); j++) {
+				if (Zu[j] == "case") {
+					caseCount++;
+				}
+				else if (Zu[j] == "}") {
+					case_counter.push_back(caseCount);
+					caseCount = 0;
+					i = j;
+					break;
+				}
+			}
+		}
+		//这里有点问题要改一下逻辑，存在嵌套的问题，switch或者if互相嵌套，考虑使用堆栈来解决
+	}
 }
 int main() {
 	InitMap();
 	string data;
 	string save;
 	string KeyGuan;
-	vector<string> zu;
 	int count = 0;
 	string test = "break";
 	int CaseCount = 0;
@@ -37,24 +61,36 @@ int main() {
 		data = infile.get();
 		save.append(data);
 	}
-	save[save.length()-1] = '\0';
+	save[save.length() - 1] = '\0';
+	infile.close();
 	for (int i = 0; i < save.length(); i++) {
-		if (MyCut(save[i])) {
+		if (MyCut(save[i]) == 1) {
 			KeyGuan.push_back(save[i]);
+		}
+		else if (MyCut(save[i]) == 2) {
+			string s(1, save[i]);
+			Zu.push_back(s);
 		}
 		else {
 			if (KeyGuan.length() > 1) {
 				cout << KeyGuan << endl;
 				if (MapKeyGuan.find(KeyGuan) != MapKeyGuan.end()) {
 					MapKeyGuan[KeyGuan] += 1;
-					zu.push_back(KeyGuan);
+					Zu.push_back(KeyGuan);
 					count++;
 				}
 			}
 			KeyGuan.clear();
 		}
 	}
+	//开始统计switch case个数和if结构总数
+	SwitchIfCounter();
 	cout << "total num: " << count << endl;
 	cout << "switch num: " << MapKeyGuan["switch"] << endl;
+	cout << "case num:";
+	for (int i = 0; i < case_counter.size(); i++) {
+		cout << " " << case_counter[i];
+	}
+	cout << endl;
 	return 0;
 }
